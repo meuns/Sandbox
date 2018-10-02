108fd3c414afd6d63a7112a1dc592d0aa7f4e4e6
Sylvain Meunier <sylvain.meunier@gmail.com>
2018-10-02 22:24:47 +0200

    Better handling of zoom (still need improvements)

--- Haha.py
+++ Haha.py
@@ -2,7 +2,7 @@ import glfw
 
 from OpenGL.GL import GL_COLOR_BUFFER_BIT, glViewport, glClear, glClearColor
 
-from glm import mat3, vec3, vec2, distance
+from glm import mat3, vec3, vec2, inverse, abs, sign
 
 
 import Random
@@ -40,6 +40,14 @@ def mat_inv_viewport(window_width, window_height):
     return mat3(2.0 / window_width, 0.0, 0.0, 0.0, -2.0 / window_height, 0.0, -1.0, +1.0, 1.0)
 
 
+def max_abs(v):
+
+    if abs(v.x) > abs(v.y):
+        return v.x
+    else:
+        return v.y
+
+
 def main():
 
     if not glfw.init():
@@ -75,51 +83,52 @@ def main():
     # Pan and zoom
     last_mouse_left = 0
     last_mouse_right = 0
-    last_cursor_left = None
-    last_view_offset = vec2(0.0, 0.0)
-    current_view_offset = vec2(0.0, 0.0)
-    last_view_zoom = 1.0
-    current_view_zoom = 0.0
-
-    last_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
-    current_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
+    last_cursor = None
+    transform_view = mat3(1.0)
+    last_view = mat3(1.0)
 
     while not glfw.window_should_close(window):
 
         window_width, window_height = glfw.get_window_size(window)
 
-        next_cursor_mat = mat_inv_projection(window_width, window_height) * mat_inv_viewport(window_width, window_height)
-        next_cursor = (next_cursor_mat * vec3(*glfw.get_cursor_pos(window), 1.0)).xy
+        projection = mat_projection(window_width, window_height)
+
+        # Pan and zoom
+        last_inv_view_projection_viewport = inverse(mat_viewport(window_width, window_height) * projection * last_view)
+        next_cursor = (last_inv_view_projection_viewport * vec3(*glfw.get_cursor_pos(window), 1.0)).xy
 
         next_mouse_left = glfw.get_mouse_button(window, 0)
         if last_mouse_left != next_mouse_left:
             if next_mouse_left:
-                last_cursor_left = next_cursor
+                last_cursor = next_cursor
             else:
-                last_view = last_view * current_view
-                current_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
+                last_view = current_view
+                transform_view = mat3(1.0)
             last_mouse_left = next_mouse_left
         else:
             if next_mouse_left:
-                delta_cursor = next_cursor - last_cursor_left
-                current_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, delta_cursor.x, delta_cursor.y, 1.0)
+                delta_cursor = next_cursor - last_cursor
+                transform_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, delta_cursor.x, delta_cursor.y, 1.0)
 
         if not next_mouse_left:
             next_mouse_right = glfw.get_mouse_button(window, 1)
             if last_mouse_right != next_mouse_right:
                 if next_mouse_right:
-                    last_cursor_left = next_cursor
+                    last_cursor = next_cursor
                 else:
-                    last_view = last_view * current_view
-                    current_view = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
+                    last_view = current_view
+                    transform_view = mat3(1.0)
                 last_mouse_right = next_mouse_right
             else:
                 if next_mouse_right:
-                    current_view_zoom = 1.0 + (next_cursor.y - last_cursor_left.y)
-                    current_view = mat3(current_view_zoom, 0.0, 0.0, 0.0, current_view_zoom, 0.0, 0.0, 0.0, 1.0)
+                    current_view_zoom = max(1.0 + max_abs(next_cursor - last_cursor), 1e-4)
+                    pivot = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, last_cursor.x, last_cursor.y, 1.0)
+                    inv_pivot = mat3(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, -last_cursor.x, -last_cursor.y, 1.0)
+                    transform_view = pivot * mat3(current_view_zoom, 0.0, 0.0, 0.0, current_view_zoom, 0.0, 0.0, 0.0, 1.0) * inv_pivot
 
         # Setup view and projection
-        view_projection = mat_projection(window_width, window_height) * (last_view * current_view)
+        current_view = last_view * transform_view
+        current_view_projection = projection * current_view
 
         # Keep random stable through frames
         Random.init(random)
@@ -128,11 +137,11 @@ def main():
         glViewport(0, 0, window_width, window_height)
         glClear(GL_COLOR_BUFFER_BIT)
 
-        World.display(world, view_projection)
+        World.display(world, current_view_projection)
 
         for iteration in range(4):
             Ray.trace(ray, iteration, world.display_buffer, random.seed_buffer)
-            Ray.display_lines(ray, view_projection, iteration)
+            Ray.display_lines(ray, current_view_projection, iteration)
 
         Ray.display_directions(ray, 0)
 
