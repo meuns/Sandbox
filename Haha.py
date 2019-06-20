fd9f77b9169f1879fcd6b4c2246c76d57cebb5d3
Sylvain Meunier <sylvain.meunier@gmail.com>
2019-06-20 23:15:52 +0200

    Show a BRDF...

--- Haha.py
+++ Haha.py
@@ -139,7 +139,7 @@ def main():
 
         World.display(world, current_view_projection)
 
-        for iteration in range(128):
+        for iteration in range(3):
             Ray.trace(ray, iteration, world.display_buffer, random.seed_buffer)
             Ray.display_lines(ray, current_view_projection, iteration)
 
